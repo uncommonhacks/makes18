4aad10187bad7a40a91d319d6cf795358129effd
Michael Noronha <michaeltnoronha@gmail.com>
2018-10-06 15:41:58 -0500

    Add a option to reset on incorrect name

--- js/script.js
+++ js/script.js
@@ -18,9 +18,17 @@ let getCookie = function(cname) {
 
 // Sets the name cookie, which is used to populate name fields on the page
 let setNameCookie = function() {
-  let name = document.getElementsByName("name")[0].value;
+  let nameInput = document.getElementsByName("name")[0];
 
-  document.cookie = "name=" + name;
+  document.cookie = "name=" + nameInput.value;
+  nameInput.value = "";
+
+  update();
+};
+
+let resetNameCookie = function() {
+  // The empty string is handled like a blank cookie
+  document.cookie = "name=";
 
   update();
 };
@@ -39,14 +47,28 @@ let update = function() {
       });
 
       // Collapse the name input section
-      let headElement = document.getElementById("head");
-      headElement.classList.add("collapsed");
+      let inputLandingElement = document.getElementById("inputLanding");
+      inputLandingElement.classList.add("collapsed");
 
       // Display greeting
       let greetingElement = document.getElementById("greeting-name");
       greetingElement.innerHTML = "Hi " + name + "!";
+
+      // Display wrong name button
+      let wrongNameElement = document.getElementById("wrong-name");
+      wrongNameElement.innerHTML = "Not " + name + "?";
   } else {
     console.log("No name cookie set, rendering the input page.");
+
+    // Collapse all sections
+    let sections = document.getElementsByClassName("section-container");
+    Array.from(sections).forEach(elem => {
+      elem.classList.add("collapsed");
+    });
+
+    // Unhide the input landing section
+    let inputLanding = document.getElementById("inputLanding");
+    inputLanding.classList.remove("collapsed");
   }
 };
 
